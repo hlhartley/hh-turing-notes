649b05a7dc6d95606d69e508adbf3effcd783a5b
Heather Hartley <hlhartley@gmail.com>
2018-11-17 13:27:17 -0700

    Add all filter button

--- main.js
+++ main.js
@@ -2,18 +2,53 @@ var htmlFilterBtn = document.querySelector('.html-filter-btn');
 var cssFilterBtn = document.querySelector('.css-filter-btn');
 var jsFilterBtn = document.querySelector('.js-filter-btn');
 var designFilterBtn = document.querySelector('.design-filter-btn');
+var allFilterBtn = document.querySelector('.all-filter-btn');
+var cardDiv = document.querySelector('.body-text');
 var cardTitle = document.querySelectorAll('.card-title');
 var cardDiv = document.querySelector('.body-text');
 
-htmlFilterBtn.addEventListener('click', filterButtons);
-cssFilterBtn.addEventListener('click', filterButtons);
-jsFilterBtn.addEventListener('click', filterButtons);
-designFilterBtn.addEventListener('click', filterButtons);
+htmlFilterBtn.addEventListener('click', filterHTML);
+cssFilterBtn.addEventListener('click', filterCSS);
+jsFilterBtn.addEventListener('click', filterJS);
+designFilterBtn.addEventListener('click', filterDESIGN);
+allFilterBtn.addEventListener('click', displayAll);
 
-function filterButtons() {
+function filterHTML() {
+    console.log('html');
   cardTitle.forEach(function(card) {
     if(!card.innerText.includes('HTML')) {
       card.closest('div').classList.add('display-none');
     }
   })
+}
+
+function filterCSS() {
+    console.log('css');
+  cardTitle.forEach(function(card) {
+    if(!card.innerText.includes('CSS')) {
+      card.closest('div').classList.add('display-none');
+    }
+  })
+}
+
+function filterJS() {
+    console.log('js');
+  cardTitle.forEach(function(card) {
+    if(!card.innerText.includes('JAVASCRIPT')) {
+      card.closest('div').classList.add('display-none');
+    }
+  })
+}
+
+function filterDESIGN() {
+  console.log('design');
+  cardTitle.forEach(function(card) {
+    if(!card.innerText.includes('DESIGN')) {
+      card.closest('div').classList.add('display-none');
+    }
+  })
+}
+
+function displayAll() {
+  location.reload();
 }
